2af0bf0dcfaa68dfa0d01e30533dea56d379e74a
jorge97710 <jazmitia_10@hotmail.com>
2019-03-23 23:18:47 -0600

    agregados graficos

--- ProyectoMineriaMorbilidad.R
+++ ProyectoMineriaMorbilidad.R
@@ -12,13 +12,13 @@ todo$numero_gen<- as.numeric(todo$genero)
 todo
 #histogramas
 hist(todo$X)
-hist(todo$AÃ.o)
+hist(todo$AÃ.o)
 hist(todo$Cantidad.total)
 #tablas de normalidad
 qqnorm(todo$X)
 qqline(todo$X, col = "steelblue", lwd = 2)
-qqnorm(todo$AÃ.o)
-qqline(todo$AÃ.o, col = "steelblue", lwd = 2)
+qqnorm(todo$AÃ.o)
+qqline(todo$AÃ.o, col = "steelblue", lwd = 2)
 qqnorm(todo$Cantidad.total)
 qqline(todo$Cantidad.total, col = "steelblue", lwd = 2)
 
@@ -45,8 +45,8 @@ margin.table(generofinal,1)
 library(e1071)#para cmeans
 library(mclust) #mixtures of gaussians
 library(fpc) #para hacer el plotcluster
-library(NbClust) #Para determinar el n�mero de clusters �ptimo
-library(factoextra) #Para hacer gr�ficos bonitos de clustering
+library(NbClust) #Para determinar el nï¿½mero de clusters ï¿½ptimo
+library(factoextra) #Para hacer grï¿½ficos bonitos de clustering
 library(cluster) #Para calcular la silueta
 library(e1071)#para cmeans
 library(mclust) #mixtures of gaussians
@@ -88,7 +88,7 @@ prop.table(table(g2$diagnostico))*100
 g3<- datos[datos$grupo==3,]
 prop.table(table(g3$diagnostico))*100
 
-plotcluster(todo[,9:15],km$cluster) #grafica la ubicación de los clusters
+plotcluster(todo[,9:15],km$cluster) #grafica la ubicaciÃ³n de los clusters
 #Error in eigen(m, symmetric = TRUE) : infinite or missing values in 'x'
 #c-means
 my_data2 <- todo[, c(9, 1, 2, 7, 3, 4, 5, 6, 8)]
@@ -96,9 +96,64 @@ fcm<-cmeans(todo[,3:4],7)
 datos$FCGrupos<-fcm$cluster
 datos<-cbind(datos,fcm$membership)
 #jerarquico
-hc<-hclust(dist(my_data2[,1:4])) #Genera el clustering jerárquico de los datos
+hc<-hclust(dist(my_data2[,1:4])) #Genera el clustering jerÃ¡rquico de los datos
 #Error: cannot allocate vector of size 2105.6 Gb
 plot(hc) #Genera el dendograma
-rect.hclust(hc,k=3) #Dibuja el corte de los grupos en el gráfico
+rect.hclust(hc,k=3) #Dibuja el corte de los grupos en el grÃ¡fico
 groups<-cutree(hc,k=3) #corta el dendograma, determinando el grupo de cada fila
 datos$gruposHC<-groups
+
+
+#Morbilidad por departamento y genero
+departamentos<- as.vector(todo$depto)
+departamentos<-unique(departamentos)
+resumen <- aggregate(todo$cantidad,by=list(todo$depto),FUN=length)
+cantidad <- as.vector(resumen[,2])
+barplot(cantidad, names.arg = departamentos,col =topo.colors(22),main = "morbilidad por departamento",las=2,cex.names = 1)
+
+
+library(ggplot2) #load ggplot2 library
+todo$numero_dep <- factor(todo$numero_dep) # Create a categorical variable
+todo$numero_gen <- factor(todo$numero_gen) # Create categorical variable
+p <- ggplot(data = todo, aes(x=numero_dep, fill=numero_gen) ) + geom_bar() # Creates stacked bar chart
+p <- p + xlab("Departamentos") + ggtitle("Genero por departamento") # Adds title and labels
+p
+
+library(reshape2)
+library(ggplot2)
+ggplot(todo, aes(x = todo$depto, y= todo$cantidad, fill = todo$genero), xlab="Age Group") +
+  geom_bar(stat="identity", width=.5, position = "dodge")  
+
+
+#CIE10 y departamento
+library(ggplot2) #load ggplot2 library
+todo$numero_dep <- factor(todo$numero_dep) # Create a categorical variable
+todo$cie <- factor(todo$cie) # Create categorical variable
+p <- ggplot(data = todo, aes(x=numero_dep, fill=cie) ) + geom_bar() # Creates stacked bar chart
+p <- p + xlab("Departamentos") + ggtitle("CIE10  por departamento") # Adds title and labels
+p
+
+#kmeans clustering para CIE y departamento
+#cluster k-medias
+
+todo$pruebacie <- todo$numero_cie
+todo$pruebadep <- todo$numero_dep
+
+datos<- todo
+todo
+todo<- na.omit(todo)
+km<-kmeans(todo[,16:17],3)
+
+datos$grupo<-km$cluster
+
+g1<- datos[datos$grupo==1,]
+prop.table(table(g1$diagnostico))*100
+nrow(g1)
+summary(g1)
+
+g2<- datos[datos$grupo==2,]
+prop.table(table(g2$diagnostico))*100
+g3<- datos[datos$grupo==3,]
+prop.table(table(g3$diagnostico))*100
+
+plotcluster(todo[,16:17],km$cluster) #grafica la ubicaciÃ³n de los clusters
