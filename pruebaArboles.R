7e9e24091c684bef1d8615c13032fa930d252e93
jorge97710 <jazmitia_10@hotmail.com>
2019-05-16 20:52:10 -0600

    Update pruebaArboles.R

--- pruebaArboles.R
+++ pruebaArboles.R
@@ -5,10 +5,11 @@ library(caret)
 library(tree)
 library(rpart.plot)
 library(randomForest)
-
+library(MASS)
+library(rpart)
 library(caTools)
 
-  
+library(party)
 
 plot(todo$Temporada,todo$clasificiacion)
 
@@ -27,7 +28,7 @@ train <- todo[muestra,] #70% entrenamiento
 test<- todo[-muestra,] #30% prueba
 
 
-tree = rpart(clasificiacion ~ AÑO + MES + SEXO + GRUPETNICO + EDAD + PERIODOEDA + DEPTORESIDEN + Temporada , data=train)
+tree = rpart(clasificiacion ~ AÃ‘O + MES + SEXO + GRUPETNICO + EDAD + PERIODOEDA + DEPTORESIDEN + Temporada , data=train)
 prp(tree)
 
 tree.pred = predict(tree, newdata=test)
@@ -37,7 +38,10 @@ tree.sse
 
 
 ########################OTRASPRUEBAS
-fit <- rpart(clasificiacion~AÑO + MES + SEXO + GRUPETNICO + EDAD + PERIODOEDA  + Temporada, data = train, method = 'class')
+fit <- rpart(clasificiacion~AÃ‘O + MES + SEXO + GRUPETNICO + EDAD + PERIODOEDA  + Temporada, data = train, method = 'class')
+
+fit <- rpart(clasificiacion~.-clasificiacion, data = train, method = 'class')
+
 rpart.plot(fit)
 
 predict_unseen <-predict(fit, test, type = 'class')
@@ -46,4 +50,21 @@ table_mat
 accuracy_Test <- sum(diag(table_mat)) / sum(table_mat)
 print(paste('Accuracy for test', accuracy_Test))
 
+#MAS PRUEBAS -------------------------------------------------------------------
 
+# Create the input data frame.
+input.dat <- todo
+
+# Give the chart file a name.
+png(file = "decision_tree.png")
+
+# Create the tree.
+output.tree <- ctree(
+  clasificiacion ~ AÃ‘O + MES + SEXO + GRUPETNICO + EDAD + PERIODOEDA  + Temporada, 
+  data = input.dat)
+
+# Plot the tree.
+plot(output.tree)
+
+# Save the file.
+dev.off()
